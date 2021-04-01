496e04523c2092fd88f5a219f92c2b56bb81e7e5
clfiedler <fiedler_clara@web.de>
2021-04-01 20:59:41 +0200

    bomb
    material, behaviorscript, put in spawnmanager, prefab

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <summary>
+/// This class in CoronaEasterGame represents a bomb that destroys the player if it gets collected
+/// </summary>
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Obect moves down
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If bomb touches player, it destroys player
+        if (other.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+        }
+
+        else if (other.CompareTag("Ground"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Collect Falling Objects
+/// <summary>
+/// This class controls the spawning of different objects in the CoronaEasterGame
+/// </summary>
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
@@ -11,12 +13,19 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private float _collectableDelay = 1f;
 
+    [SerializeField]
+    private GameObject _bombPrefab;
+
+    [SerializeField]
+    private float _bombDelay = 5f;
+
     private bool _spawningON = true;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(routine: CollectableSpawnSystem());
+        StartCoroutine(routine: BombSpawnSystem());
     }
 
     // Update is called once per frame
@@ -34,6 +43,17 @@ public class SpawnManager : MonoBehaviour
             // wait for delay
             yield return new WaitForSeconds(_collectableDelay);
         }
-    }        
+    }
+
+    IEnumerator BombSpawnSystem()
+    {
+        // spawn new collectibles 
+        while (_spawningON)
+        {
+            Instantiate(_bombPrefab, position: new Vector3(x: Random.Range(-9.5f, 9.5f), y: 10f, z: 0f), Quaternion.identity, this.transform);
+            // wait for delay
+            yield return new WaitForSeconds(_bombDelay);
+        }
+    }
 
 }
